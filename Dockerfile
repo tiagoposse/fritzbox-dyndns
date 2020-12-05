23ab14274d97186c40af76b7a63a44f990f99037
Tiago Posse <tiagoposse@gmail.com>
2020-12-05 09:43:13 +0100

    v2: get external ipv6 from ipv4 address or hostname target, trigger drone on push request

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM arm64v8/python:3.9-alpine
 
+RUN apk update && apk add bind-tools
+
 COPY main.py requirements.txt /app/
 COPY providers/ /app/providers
 
 RUN pip install -r /app/requirements.txt
 
 ENTRYPOINT [ "python" ]
+
 CMD [ "/app/main.py" ]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import yaml, os, sys
+import yaml, os, re, subprocess, sys
 import xml.etree.ElementTree as ET
 
 os.chdir(os.path.realpath(os.path.dirname(sys.argv[0])))
@@ -16,12 +16,25 @@ def main():
     exit()
 
   fc = FritzConnection(conf['fritzbox']['address'])
-  if conf['externalIPv6Suffix'] != "":
-    ipv6_prefix = fc.call_action('WANIPConn1', 'X_AVM_DE_GetIPv6Prefix')
-    external_ipv6 = ':'.join(ipv6_prefix['NewIPv6Prefix'].split(':')[0:4]) + ':' + conf['externalIPv6Suffix']
+  if conf['ipv6']['enabled']:
+    if conf['ipv6']['target'] == '':
+      print('IPv6 is enabled but not target host has been set.')
+      exit()
+
+    ipv6_prefix = ':'.join(fc.call_action('WANIPConn1', 'X_AVM_DE_GetIPv6Prefix')['NewIPv6Prefix'].split(':')[0:4])
+
+    process = subprocess.Popen(
+      f"/usr/bin/host -6 -v { conf['ipv6']['target'] } { re.sub(r'https?://', '', conf['fritzbox']['address']) }".split(' '),
+      stdout=subprocess.PIPE, 
+      stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+
+    external_ipv6 = re.search(fr"{ ipv6_prefix }.+", stdout.decode('utf-8')).group(0)
   else:
     external_ipv6 = ''
 
+  # print(external_ipv6)
   external_ipv4 = fc.call_action('WANIPConn1', 'GetExternalIPAddress')['NewExternalIPAddress']
 
   if 'namecheap' in conf:
